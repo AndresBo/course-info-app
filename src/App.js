f400d4e17c66b7db372eb5aaafbeb74f7f9d0a17
AndresBo <anndresfborrero@gmail.com>
2023-06-19 15:16:49 +1000

    build: modified App component to use map function. Also using Nanoid to create key for each child in array

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
-const Header = ({course}) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
 
-const Content = ({parts}) => {
-  const listParts = parts.map(part => <p>{part.name} {part.exercises}</p>)
+const App = (props) => {
+  const { notes } = props
 
-  return (
-    <div>{listParts}</div>
-  )
-}
-
-const Total = ({parts}) => {
-  
-  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises; 
-  return (
-    <p>Number of exercises {totalExercises}</p>
-  )
-}
-
-
-
-
-const App = () => {
-  
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts}/>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+        <li key={note.id}>
+          {note.content}
+        </li>
+        )}
+      </ul>
     </div>
   )
 }

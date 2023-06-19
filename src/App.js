a597bd86adec7c7860fb8d75ec9e7ad752962da7
AndresBo <anndresfborrero@gmail.com>
2023-06-19 18:15:30 +1000

    build: moved Note component to a components folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
+import Note from "./components/Note"
 
 const App = ({notes}) => {
   
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-        <li key={note.id}>
-          {note.content}
-        </li>
-        )}
-      </ul>
-    </div>
+return (
+  <div>
+    <h1>Notes</h1>
+    <ul>
+      {notes.map(note => 
+      <Note key={note.id} note={note}/>
+      )}
+    </ul>
+  </div>
   )
 }
 

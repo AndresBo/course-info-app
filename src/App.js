44280218cd3cde68847a296777109172b8b2b2de
AndresBo <anndresfborrero@gmail.com>
2023-06-21 09:33:00 +1000

    build: added state notes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Note from "./components/Note"
+import { useState } from "react"
 
-const App = ({notes}) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
   
-return (
-  <div>
-    <h1>Notes</h1>
-    <ul>
-      {notes.map(note => 
-      <Note key={note.id} note={note}/>
-      )}
-    </ul>
-  </div>
-  )
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+        <Note key={note.id} note={note}/>
+        )}
+      </ul>
+    </div>
+    )
 }
 
 export default App

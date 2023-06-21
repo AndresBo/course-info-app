3d4d5e4307c14b13f5dd225029835a370a7a4acb
AndresBo <anndresfborrero@gmail.com>
2023-06-21 10:33:22 +1000

    build: added state showAll, a boolean state that allows filtering notes using the important attribute

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { nanoid } from "nanoid"
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -21,12 +22,14 @@ const App = (props) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
   
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
         <Note key={note.id} note={note}/>
         )}
       </ul>

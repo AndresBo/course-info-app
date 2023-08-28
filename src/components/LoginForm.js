99f32b4e64af950a3aad0868bb17b877525e01bd
AndresBo <anndresfborrero@gmail.com>
2023-08-28 10:58:13 +1000

    build: added CSS id to login button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -36,7 +36,7 @@ const LoginForm = ({ handleSubmit }) => {
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button id='login-button' type="submit">login</button>
       </form>
     </div>
   )

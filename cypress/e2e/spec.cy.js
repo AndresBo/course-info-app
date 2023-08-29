b350eb615b79a0ffd226b00eb0ad1aac0f95ee33
AndresBo <anndresfborrero@gmail.com>
2023-08-29 11:32:09 +1000

    test: adding cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,70 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
+    // reset test db and create a new user
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const user = {
+      name: 'administrator',
+      username: 'admin',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('Notes')
+  })
+
+  it('user can login', function(){
+    cy.contains('log in').click()
+    cy.get('#username').type('admin')
+    cy.get('#password').type('password')
+    cy.get('#login-button').click()
+    cy.contains('administrator logged in')
+  })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.visit('http://localhost:3000')
+      cy.contains('log in').click()
+      cy.get('#username').type('admin')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function () {
+      cy.contains('new note').click()
+      cy.get('#new-note').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exists', function() {
+      beforeEach(function() {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('can be made not important', function() {
+        cy.contains('another note cypress')
+          .contains('make note not important')
+          .click()
+
+        cy.contains('another note cypress')
+          .contains('make note important')
+      })
+    })
+  })
+
+  it('login fails with wrong password', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('admin')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error').contains('Wrong credentials')
+    cy.get('html').should('not.contain', 'administrator logged in')
   })
 })
